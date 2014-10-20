d327ca2a13ceeca634ed5da0f75d2225361199c2
rmichie <michie.ryan@deq.state.or.us>
2014-10-19 22:54:44 -0700

    update

--- Rcode_ssn.R
+++ Rcode_ssn.R
@@ -1,37 +1,116 @@
 
 
-
 library(party)
+library(reshape)
 
 options(stringsAsFactors = FALSE)
 vars <- read.csv("VarNames_RF.csv")
 bugs <- read.csv("ssn_RF_data.csv")
 
 # -----------------------------------------------------------
-# FSS - Random forests
-vars.fss <- vars[vars$fss2.rf_keep == 1,]
-fss <- bugs[,colnames(bugs) %in% vars.fss$var]
+# FSS2 - Random forests excluding the physical habitat data
+
+# -----------------------------------------------------------
+# Step 1, per Genuer et al (2000), use brute force to identify important variables. 
+# Since we have a large amount of variables random forest is run 50 times with a 
+# very high number of trees per forest (ntree). This yields a distribution of importance scores.
+# Removal is based on these distributions. We keep the variables with the highest scores.
+
+vars.fss2.s1 <- vars[vars$fss2.rf_keep == 1,]
+fss2.s1 <- bugs[,colnames(bugs) %in% vars.fss2.s1$var]
 
 # remove NAs in response variable
-fss <- fss[(!is.na(fss$FSS_26Aug14)),]
-fss.na <- data.frame(na.omit(fss))
+fss2.s1 <- fss2.s1[(!is.na(fss2.s1$FSS_26Aug14)),]
 
-colnames(fss.na)
+colnames(fss2.s1)
 
-# factor chr vars
-#fss.na$YEAR <- factor(fss.na$YEAR)
+# mtry and ntree values 
+mtry.fss2.s1 <- as.integer(((ncol(fss2.s1)-1) / 3),0)
 
+# initialize the variable importance df
+fss2.s1.vi <- data.frame(matrix(, nrow = ncol(fss2.s1)-1, ncol = 50))
+fss2.s1.col <- colnames(fss2.s1)
+fss2.s1.col <- fss2.s1.col[!(fss2.s1.col == "FSS_26Aug14")]
+
+# WARNING - Takes a long time to run.
 set.seed(42)
-fss.cf <- cforest(FSS_26Aug14 ~ ., data = fss,controls = cforest_unbiased(ntree = 50, stump = TRUE))
-fss.ct <- ctree(FSS_26Aug14 ~ ., data = fss, controls = ctree_control(maxsurrogate = 3))
+for (i in 1:50) {
+  print(i)
+  fss2.s1.cf <- cforest(FSS_26Aug14 ~ ., data = fss2.s1, controls = cforest_unbiased(ntree = 2000, mtry = mtry.fss))
+  fss2.s1.vi[,i]<- varimp(fss2.s1.cf, conditional=FALSE)
+}
 
+# Add var names and index
+fss2.s1.vi[,51]<- fss.col
+fss2.s1.vi[,52]<-c(1:length(fss.col))
+colnames(fss2.s1.vi)[51] <- "var_name"
+colnames(fss2.s1.vi)[52] <- "var_index"
+
+# ----------
+# Save the df with a timestamp so we don't accidently overwrite it.
+timestamp <- format(Sys.time(), "%Y%m%d_%H%M")
+save(fss2.s1.vi, file=paste0("fss2_vi_s1_",timestamp,".RData"))
+
+load("fss2_vi_s1_20141019_1451.RData")
+# ----------
+
+fss2.s1.vi.l <- melt(fss2.s1.vi, id=c("var_name","var_index"))
+
+bymedian <- with(fss2.s1.vi.l, reorder(var_index, -value, median))
+boxplot(value ~ bymedian, data = fss2.s1.vi.l,
+        xlab = "Variable index", ylab = "Importance", 
+        varwidth = TRUE,
+        col = "lightgray")
+
+fss2.s1.vi.median <- cast(fss2.s1.vi.l,var_name + var_index ~ ., value ='value', median)
+colnames(fss2.s1.vi.median )[3] <- "median"
+
+# sort the data so largest at the top
+fss2.s1.vi.median <- fss2.s1.vi.median[with(fss2.s1.vi.median, order(-median)), ]
+
+# Variable removal
+# After the first 15 largest median values starts to flatten out so 
+# we will take the top 30 to step 2.
+
+# grab all variable names with median values > 0.5
+fss2.s2.col <- fss2.s1.vi.median[fss2.s1.vi.median$median > 0.5,][,1]
+fss2.s2.col <- c("FSS_26Aug14",fss2.s2.col)
+fss2.s2 <- fss2.s1[,colnames(fss2.s1) %in% fss2.s2.col]
+
+# remove any NAs
+fss2.s2 <- data.frame(na.omit(fss2.s2))
+
+# -----------------------------------------------------------
+# STEP 2. Here we follow reccomendations by Strobl et al (2008) and Strobl et al (2009) 
+# and calculate conditional variable importance. This reduces importance scores on 
+# variables that get hight socres from step 1 becuase they highly correlated to 
+# to other ones. Probably applies to some of the climate variables.
+# We couldn't do this in step 1 becuase we had too many variables and NAs.
+# Not enough computer memory. A smaller dataset is ideal.
+
+# initialize the variable importance df
+fss2.s2.vi <- data.frame(matrix(, nrow = ncol(fss2.s2)-1, ncol = 50))
+fss2.s2.col <- colnames(fss2.s2)
+fss2.s2.col <- fss2.s2.col[!(fss2.s2.col == "FSS_26Aug14")]
+
+# mtry and ntree values 
+mtry.fss2.s2 <- as.integer(((ncol(fss2.s2)-1) / 3),0)
+
+set.seed(84)
+#for (i in 1:50) {
+  #print(i)
+ #fss2.s2.cf <- cforest(FSS_26Aug14 ~ ., data = fss2.s2,controls = cforest_unbiased(ntree = 2000, mtry = mtry.fss2.s2))
+#fss2.s2.vi[,i]<- varimp(fss2.s2.cf, conditional=TRUE)
+#}
+
+fss2.s2.cf <- cforest(FSS_26Aug14 ~ ., data = fss2.s2,controls = cforest_unbiased(mtry = mtry.fss2.s2))
+test <- varimp(fss2.s2.cf, conditional=TRUE)
+
+# -----------------------------------------------------------
+
+fss.ct <- ctree(FSS_26Aug14 ~ ., data = fss, controls = ctree_control(maxsurrogate = 3))
 plot(fss.ct)
 
-df <- data.frame(sort(varimp(fss.cf, conditional=FALSE),decreasing = TRUE))
-
-set.seed(99998)
-
-plot(fss.cf)
 
 # -----------------------------------------------------------
 library(randomForest)
