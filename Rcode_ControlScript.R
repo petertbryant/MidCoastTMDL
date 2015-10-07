653504e4b25cb80a21a5dd800b0e40ca96f6486e
petertbryant <bryant.peter@deq.state.or.us>
2015-10-07 14:52:17 -0700

    Renamed files to make it easier to tell the order they need to be run.

--- Rcode_ControlScript.R
+++ Rcode_ControlScript.R
@@ -1,19 +1,21 @@
 #Control script to run random forest, correlations, model selection and model averaging
 
-#source('R_code_combine_data.R')
+#source('01_combine_data.R')
 #Inputs: Calculated watershed characteristics. Mostly stored in LSN04/Tables.mdb. Some stored in various .csv
 #Outputs: VarNames_RF_V2.csv, ssn_RF_data.csv
 
-#source('R_code_RF_FSS2.R')
+#source('02_random_forest_step1.R')
 #Inputs: VarNames_RF_V2.csv, ssn_RF_data.csv
 #Outputs: fss2_s1_vi_20150720_1627.RData = First run variable importance scores for all 50 runs 
 #         fss2_s1_visd_20150720_1627.RData = First run variable importance standard deviations
 #         fss2_s1_vi_median_20150722_1035.RData = Median importance values as based on the 50 runs
 #         fss2_s1_20150722_1035.RData = The edited data used as input to randomForest function
 
-#Placeholder for breakout of correlation variable selection step
+#source('03_variable_selection.R')
 #Inputs: fss2_s1_vi_median_20150722_1035.RData, fss2_s1_20150722_1035.RData 
 #Outputs: fss2_s2_data.csv
+
+#source('04_random_forest_step2.R')
 
 #source('Rcode_transformations.R')
 #Inputs: ssn_RF_data.csv, fss_s2_data.csv, ssn object, 
